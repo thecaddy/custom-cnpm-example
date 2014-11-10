4600680b99d2fcdc0628c03bb2287684fa1ec25d
Joel Trost <joelt@zillow.com>
2014-11-10 13:39:28 -0800

    confg.js

--- hello_cnpm/config.js
+++ hello_cnpm/config.js
@@ -4,15 +4,33 @@ module.exports = {
     'admin': 'admin@cnpmjs.org'
   },
   bindingHost: '',
-  mysqlServers: [
-  {
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: '',
-  }
-  database:{
+    database: {
     db: 'cnpmjs_test',
-    dialect: 'mysql'
-  }
+    username: 'root',
+    password: '',
+
+    // the sql dialect of the database
+    // - currently supported: 'mysql', 'sqlite', 'postgres', 'mariadb'
+    dialect: 'mysql',
+
+    // custom host; default: 127.0.0.1
+    host: '127.0.0.1',
+
+    // custom port; default: 3306
+    port: 3306,
+
+    // use pooling in order to reduce db connection overload and to increase speed
+    // currently only for mysql and postgresql (since v1.5.0)
+    pool: {
+      maxConnections: 10,
+      minConnections: 0,
+      maxIdleTime: 30000
+    },
+
+    // the storage engine for 'sqlite'
+    // default store into ~/cnpmjs.org.sqlite
+    storage: path.join(process.env.HOME || root, 'cnpmjs.org.sqlite'),
+
+    //logging: !!process.env.SQL_DEBUG,
+  },
 };
